8c5236a6a4ab37a122ef970f1a0d96cb8e94da53
Ajitesh Madai <ajitesh.madai@gmail.com>
2015-12-15 16:15:34 +0530

    Added func to handle different types of Questions

--- js/questionController.js
+++ js/questionController.js
@@ -4,22 +4,31 @@
 
 var questions = [ {
 	question : 'What is the capital of Japan?',
-	answers : [ 'Tokyo', 'Delhi', 'Sydney', 'London' ]
+	answers : [ 'Tokyo', 'Delhi', 'Sydney', 'London' ],
+	isOptional : false,
+	isMutliChoice : true
 },
 
 {
 	question : 'What is the capital of Denmark?',
-	answers : [ 'Tokyo', 'Delhi', 'Copenhagen', 'London' ]
+	answers : [ 'Tokyo', 'Delhi', 'Copenhagen', 'London' ],
+	isOptional : true,
+	isMutliChoice : false
+},
+
+{
+	question : 'Tell us something about youself..',
+	isOptional: false
 }
 
 ];
 
 quizApp.controller("QuestionController", function() {
-
-	this.currentQuestion = questions[0];	
+	var questionCount = 0;
+	this.currentQuestion = questions[questionCount];	
 	this.getNextQuestion = function() {
 		
-		this.currentQuestion = questions[1];
+		this.currentQuestion = questions[questionCount++];
 	};
 
 });
